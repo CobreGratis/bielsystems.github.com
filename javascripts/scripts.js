118d2969756859823afd75d364e5d4190468be8e
Alexandre Benitah <fbazzarella@MacBook-Pro-de-Alexandre.local>
2012-10-17 18:18:31 -0300

    Some Zombie Fixes in scroll and subscrption =)

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("a[rel^='external']").each(function() { $(this).attr("target","_blank"); });
 
-  $('nav, header').localScroll({
+  $('nav, header, #right').localScroll({
     duration: 1000,
     axis: 'y'
   });
