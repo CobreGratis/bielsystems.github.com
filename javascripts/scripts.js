26984bfef524573781c553ef27a03ef2f9c4fc9c
Aluísio Azevedo <aluisio.me@gmail.com>
2012-11-16 17:34:32 -0200

    Improving mobile view

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,3 +1,20 @@
+$(document).ready(function(){
+  $("a[rel^='external']").each(function() { $(this).attr("target","_blank"); });
+
+  $('nav, header, #right, #single-page').localScroll({
+    duration: 1000,
+    axis: 'y'
+  });
+
+  $(window).scroll(function(){
+    var posX = $(window).scrollTop() - ($(window).scrollTop() * 40/100) + "px";
+    $("#home").css({backgroundPosition: "50% " + posX });
+  });
+
+});
+
+
+
 /**
  * Parallax Scrolling Tutorial
  * For NetTuts+
