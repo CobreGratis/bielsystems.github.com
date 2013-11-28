ee112a1d4f704965c1fbd8f0f589de5b69cee094
Ali Ismayilov <ali@ismailov.info>
2013-11-28 17:02:09 -0200

    Blogpost#fetch atom feed

--- lib/blogpost.rb
+++ lib/blogpost.rb
@@ -1,3 +1,25 @@
+require 'open-uri'
+require 'nokogiri'
+
 class Blogpost
-  
+  attr_accessor :title, :url, :author
+
+  def initialize(title, url, author)
+    @title  = title
+    @url    = url
+    @author = author
+  end
+
+  def self.fetch(url)
+    doc = Nokogiri::XML(open(url))
+    doc.css('entry').map do |entry|
+      Blogpost.new(entry.css('title').text, entry.css('id').text, entry.css('name').text)
+    end
+  end
+
+  def ==(other)
+    self.title  == self.title &&
+    self.url    == self.url &&
+    self.author == other.author
+  end
 end
--- spec/blogpost_spec.rb
+++ spec/blogpost_spec.rb
@@ -1,7 +1,13 @@
+# -*- encoding : utf-8 -*-
 require 'blogpost'
 
 describe Blogpost do
-  it "true is false" do
-    expect(true).to be_false
+  describe "#fetch" do
+    let(:blogpost) { Blogpost.new('Redesign do Startupdev', 'http://helabs.com.br/blog/2013/11/26/startupdev-novo-design', 'Aluísio Azevedo') }
+
+    it "returns array of blogposts" do
+      entries = Blogpost.fetch('http://helabs.com.br/blog/atom.xml')
+      expect(entries).to include(blogpost)
+    end
   end
 end
