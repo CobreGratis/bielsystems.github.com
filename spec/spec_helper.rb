cba8123943a51c9f028d0bf3b667cd377c98f114
Ali Ismayilov <ali@ismailov.info>
2013-12-03 11:12:54 -0200

    use vcr for Blogpost#fetch

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,13 @@
 require 'blogpost'
-# require 'vcr'
+require 'vcr'
 require 'jekyll'
 require 'pry'
 
-# TODO: use VCR
-# VCR.configure do |c|
-#   c.cassette_library_dir = 'spec/cassettes'
-#   c.hook_into :webmock
-#   c.configure_rspec_metadata!
-# end
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
 
 RSpec.configure do |c|
   # so we can use :vcr rather than :vcr => true;
