c26ec88d2a54f1fc9be3c3f54c5537e91e1e804e
Rafael Santos <estradeiro@gmail.com>
2014-01-02 16:50:13 -0200

    include javascripts

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 compass compile
 cd javascripts
 rm ./javascripts.min.js
-cat jquery.scrollTo.js jquery.localScroll.js jquery.prettyPhoto.js scripts.js > javascripts.js
+cat jquery.maskedinput.js jquery.validate.js jquery.scrollTo.js jquery.localScroll.js jquery.prettyPhoto.js scripts.js > javascripts.js
 java -jar ../tools/yuicompressor-2.4.8.jar ./javascripts.js -o ./javascripts.min.js -v
 rm ./javascripts.js
 cd ../
