507e9cff72eeec1692ed89b4a03d1a409cdbb2a3
Matheus Bras <bras.matheus@gmail.com>
2014-01-02 10:14:45 -0200

    Add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      sass: {
+        files: ['sass/**'],
+        tasks: ['sass'],
+        options: {
+          livereload: true
+        }
+      },
+      js: {
+        files: ['javascripts/**'],
+        tasks: ['uglify'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'style.css': 'sass/style.scss'
+        }
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'javascripts/application.min.js': ['javascripts/source/**.js']
+        }
+      }
+    }
+  });
+
+  //Load NPM tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  //Making grunt default to force in order not to break the project.
+  grunt.option('force', true);
+
+  //Default task(s).
+  grunt.registerTask('default', ['sass', 'uglify', 'watch']);
+};
