83794ed99ebce076ee3858b97df2ae1f09bb08c9
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2014-01-03 12:57:59 -0200

    Changed Trabalhos to Trabalho

---
+++ _site/javascripts/scripts.js
@@ -0,0 +1,273 @@
+// Collapse Plugin
+
+(function($) {
+  $.fn.collapsable = function(options) {
+    // iterate and reformat each matched element
+    return this.each(function() {
+      // cache this:
+      var obj = $(this);
+      var tree = obj.next('.navigation');
+      obj.click(function() {
+        if (obj.is(':visible')) {
+          tree.toggle();
+        }
+      });
+      $(window).resize(function() {
+        if ($(window).width() <= 570) {
+          tree.attr('style', '');
+        };
+      });
+    });
+  };
+})(jQuery);
+
+
+// Functions
+
+var App = {
+
+  StartApp: function() {
+    this.Modal();
+    this.InterfaceActions();
+  },
+
+  Modal: function() {
+
+    var
+      $modalBg = $("#modalBg"),
+      $modalBox = $(".modalBox")
+    ;
+
+    $modalBox.append('<span class="modalClose">Fechar</span>');
+
+    function EnableModal() {
+      $('body').css({
+        'overflow': 'hidden'
+      });
+      $modalBg.show();
+    }
+
+    function DisableModal() {
+      $modalBg.fadeOut();
+      $modalBox.fadeOut();
+      $("body").css("overflow", "visible");
+    }
+
+    // Trigger 01
+    $('.modalTrigger').click(function() {
+      EnableModal();
+      $(this).parent().parent().find('.modalBox').fadeIn();
+    });
+
+    // Trigger 02
+    $('.m-det').click(function() {
+      EnableModal();
+      $(this).parent().find('.modalBox').fadeIn();
+    });
+
+    $modalBg.mouseup(function() {
+      DisableModal();
+    });
+
+    $('.modalClose').click(function() {
+      DisableModal();
+    });
+
+  }, // Modal
+
+  InterfaceActions: function() {
+
+    // Eduardo's easter egg
+    $("img[src='/images/time/time-eduardo.jpg']").hover(function() {
+      $(this).attr("src", "/images/time/time-eduardo.gif");
+    });
+
+    // contact form
+    $("#budget").mask("R$ ?999999");
+
+    $("#phone").focusout(function(){
+      var phone, element;
+      element = $(this);
+      element.unmask();
+      phone = element.val().replace(/\D/g, '');
+      if(phone.length > 10) {
+          element.mask("(99) 99999-999?9");
+      } else {
+          element.mask("(99) 9999-9999?9");
+      }
+    }).trigger('focusout');
+
+    $("#talk-to-us").validate({
+      rules: {
+        Field3: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        Field18: "Nome é obrigatório",
+        Field19: "Sobrenome é obrigatório",
+        Field3: "Email inválido",
+        Field16: "Telefone é obrigatório",
+        Field21: "Orçamento é obrigatório",
+        Field5: "Resumo do projeto é obrigatório"
+      }
+    });
+
+    //
+    $('.hide-content').addClass("hidden");
+
+    $('.hide-content').click(function() {
+      var $this = $(this);
+
+      if ($this.hasClass("hidden")) {
+        $(this).removeClass("hidden").addClass("visible");
+
+      } else {
+        $(this).removeClass("visible").addClass("hidden");
+      }
+    });
+
+    /* Menu: Current Page */
+
+    var current_page = location.pathname;
+
+    $("#menu2 a").each(function(i) {
+      if (current_page.indexOf($(this).attr('href')) == 0) {
+        $(this).addClass('current-page');
+        return false
+      }
+    });
+
+
+    /* Menu: Mobile */
+
+    $("#menu-trigger").click(function() {
+      $("#menu2").toggle();
+    });
+
+    $('.slide-trigger').collapsable();
+
+
+    /* PrettyPhoto */
+
+    $("a[rel^='prettyPhoto']").prettyPhoto({
+      deeplinking: false,
+      changepicturecallback: function() {
+        var viewportWidth = $('body').innerWidth();
+        if (viewportWidth < 1025) {
+          $(".pp_pic_holder.pp_default").css("top", window.pageYOffset + "px");
+        }
+      }
+    });
+
+
+    /* External Links */
+
+    $("a[rel^='external']").each(function() {
+      $(this).attr("target", "_blank");
+    });
+
+
+    /* General */
+
+    $('nav, .advice, header, #right, #single-page').localScroll({
+      duration: 1000,
+      axis: 'y'
+    });
+
+    $(window).scroll(function() {
+      if ($(window).width() > 960) {
+        var posX = $(window).scrollTop() - ($(window).scrollTop() * 40 / 100) + "px";
+        $("#home").css({
+          backgroundPosition: "50% " + posX
+        });
+      }
+    });
+
+    if ($("section#cloud").size() > 0) {
+
+      var
+      $slider = $("ul.step-list"),
+        i = 1;
+
+      function changeActive() {
+        $("ul.nav-slider a").removeClass("active").eq(i - 1).addClass("active");
+      }
+
+      function animateSlider(index) {
+        $slider.animate({
+          left: "-" + 930 * i + "px"
+        }, 1000);
+      }
+
+      $("ul.nav-slider li").click(function(e) {
+        e.preventDefault();
+        i = $(this).index();
+        animateSlider(i);
+        i = $(this).index() + 1;
+        changeActive();
+      });
+
+      $("#next").click(function(e) {
+        e.preventDefault();
+        if (i <= 2) {
+          i++;
+          $slider.animate({
+            left: "-=930px"
+          }, 1000);
+        } else {
+          i = 1;
+          $slider.animate({
+            left: "0px"
+          }, 1000);
+        }
+
+        changeActive();
+      });
+
+      $("#prev").click(function(e) {
+        e.preventDefault();
+        if (i > 1) {
+          $slider.animate({
+            left: "+=930px"
+          }, 1000);
+          i--;
+        } else if (i <= 1) {
+          $slider.animate({
+            left: "-=" + 930 * 2 + "px"
+          }, 1000);
+          i = 5;
+        }
+        changeActive();
+      });
+    }
+
+    if ($("section#cliente").size() > 0) {
+
+      var
+        link = $(".js-client-videos-chooser a"),
+        iframe = $(".js-client-videos iframe")
+      ;
+
+      iframe.hide();
+      iframe.eq(0).show();
+
+      link.eq(0).addClass("selected");
+      link.click(function(e) {
+        var clicked = $(this).index();
+        link.removeClass("selected");
+        $(this).addClass("selected");
+        iframe.hide();
+        iframe.eq(clicked).show();
+        e.preventDefault();
+      });
+    }
+  }
+
+
+} // Var Site
+
+jQuery(function() {
+  App.StartApp();
+});
---
+++ _site/serve/server/javascripts/jquery.scrollTo.js
@@ -0,0 +1,57 @@
+/*
+ * jQuery.ScrollTo
+ * Copyright (c) 2007-2009 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
+ * Dual licensed under MIT and GPL.
+ * Date: 06/05/2009
+ *
+ * @projectDescription Easy element scrolling using jQuery.
+ * http://flesler.blogspot.com/2007/10/jqueryscrollto.html
+ * Works with jQuery +1.2.6. Tested on FF 2/3, IE 6/7/8, Opera 9.5/6, Safari 3, Chrome 1 on WinXP.
+ *
+ * @author Ariel Flesler
+ * @version 1.4.2
+ *
+ * @id jQuery.scrollTo
+ * @id jQuery.fn.scrollTo
+ * @param {String, Number, DOMElement, jQuery, Object} target Where to scroll the matched elements.
+ *	  The different options for target are:
+ *		- A number position (will be applied to all axes).
+ *		- A string position ('44', '100px', '+=90', etc ) will be applied to all axes
+ *		- A jQuery/DOM element ( logically, child of the element to scroll )
+ *		- A string selector, that will be relative to the element to scroll ( 'li:eq(2)', etc )
+ *		- A hash { top:x, left:y }, x and y can be any kind of number/string like above.
+ *		- A percentage of the container's dimension/s, for example: 50% to go to the middle.
+ *		- The string 'max' for go-to-end. 
+ * @param {Number, Function} duration The OVERALL length of the animation, this argument can be the settings object instead.
+ * @param {Object,Function} settings Optional set of settings or the onAfter callback.
+ *	 @option {String} axis Which axis must be scrolled, use 'x', 'y', 'xy' or 'yx'.
+ *	 @option {Number, Function} duration The OVERALL length of the animation.
+ *	 @option {String} easing The easing method for the animation.
+ *	 @option {Boolean} margin If true, the margin of the target element will be deducted from the final position.
+ *	 @option {Object, Number} offset Add/deduct from the end position. One number for both axes or { top:x, left:y }.
+ *	 @option {Object, Number} over Add/deduct the height/width multiplied by 'over', can be { top:x, left:y } when using both axes.
+ *	 @option {Boolean} queue If true, and both axis are given, the 2nd axis will only be animated after the first one ends.
+ *	 @option {Function} onAfter Function to be called after the scrolling ends. 
+ *	 @option {Function} onAfterFirst If queuing is activated, this function will be called after the first scrolling ends.
+ * @return {jQuery} Returns the same jQuery object, for chaining.
+ *
+ * @desc Scroll to a fixed position
+ * @example $('div').scrollTo( 340 );
+ *
+ * @desc Scroll relatively to the actual position
+ * @example $('div').scrollTo( '+=340px', { axis:'y' } );
+ *
+ * @desc Scroll using a selector (relative to the scrolled element)
+ * @example $('div').scrollTo( 'p.paragraph:eq(2)', 500, { easing:'swing', queue:true, axis:'xy' } );
+ *
+ * @desc Scroll to a DOM element (same for jQuery object)
+ * @example var second_child = document.getElementById('container').firstChild.nextSibling;
+ *			$('#container').scrollTo( second_child, { duration:500, axis:'x', onAfter:function(){
+ *				alert('scrolled!!');																   
+ *			}});
+ *
+ * @desc Scroll on both axes, to different values
+ * @example $('div').scrollTo( { top: 300, left:'+=200' }, { axis:'xy', offset:-20 } );
+ */
+
+;(function($){var h=$.scrollTo=function(a,b,c){$(window).scrollTo(a,b,c)};h.defaults={axis:'xy',duration:parseFloat($.fn.jquery)>=1.3?0:1,limit:true};h.window=function(a){return $(window)._scrollable()};$.fn._scrollable=function(){return this.map(function(){var a=this,isWin=!a.nodeName||$.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!=-1;if(!isWin)return a;var b=(a.contentWindow||a).document||a.ownerDocument||a;return/webkit/i.test(navigator.userAgent)||b.compatMode=='BackCompat'?b.body:b.documentElement})};$.fn.scrollTo=function(e,f,g){if(typeof f=='object'){g=f;f=0}if(typeof g=='function')g={onAfter:g};if(e=='max')e=9e9;g=$.extend({},h.defaults,g);f=f||g.duration;g.queue=g.queue&&g.axis.length>1;if(g.queue)f/=2;g.offset=both(g.offset);g.over=both(g.over);return this._scrollable().each(function(){if(e==null)return;var d=this,$elem=$(d),targ=e,toff,attr={},win=$elem.is('html,body');switch(typeof targ){case'number':case'string':if(/^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(targ)){targ=both(targ);break}targ=$(targ,this);if(!targ.length)return;case'object':if(targ.is||targ.style)toff=(targ=$(targ)).offset()}$.each(g.axis.split(''),function(i,a){var b=a=='x'?'Left':'Top',pos=b.toLowerCase(),key='scroll'+b,old=d[key],max=h.max(d,a);if(toff){attr[key]=toff[pos]+(win?0:old-$elem.offset()[pos]);if(g.margin){attr[key]-=parseInt(targ.css('margin'+b))||0;attr[key]-=parseInt(targ.css('border'+b+'Width'))||0}attr[key]+=g.offset[pos]||0;if(g.over[pos])attr[key]+=targ[a=='x'?'width':'height']()*g.over[pos]}else{var c=targ[pos];attr[key]=c.slice&&c.slice(-1)=='%'?parseFloat(c)/100*max:c}if(g.limit&&/^\d+$/.test(attr[key]))attr[key]=attr[key]<=0?0:Math.min(attr[key],max);if(!i&&g.queue){if(old!=attr[key])animate(g.onAfterFirst);delete attr[key]}});animate(g.onAfter);function animate(a){$elem.animate(attr,f,g.easing,a&&function(){a.call(this,e,g)})}}).end()};h.max=function(a,b){var c=b=='x'?'Width':'Height',scroll='scroll'+c;if(!$(a).is('html,body'))return a[scroll]-$(a)[c.toLowerCase()]();var d='client'+c,html=a.ownerDocument.documentElement,body=a.ownerDocument.body;return Math.max(html[scroll],body[scroll])-Math.min(html[d],body[d])};function both(a){return typeof a=='object'?a:{top:a,left:a}}})(jQuery);
