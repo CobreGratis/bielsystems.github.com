d04f008ba76bbb17bde5b06bec7601f7d8218423
Matheus Bras <bras.matheus@gmail.com>
2014-01-03 13:44:22 -0200

    Removed ignored folders

--- _site/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-compass compile
-cd javascripts
-rm ./javascripts.min.js
-cat jquery.maskedinput.js jquery.validate.js jquery.scrollTo.js jquery.localScroll.js jquery.prettyPhoto.js scripts.js > javascripts.js
-java -jar ../tools/yuicompressor-2.4.8.jar ./javascripts.js -o ./javascripts.min.js -v
-rm ./javascripts.js
-cd ../
