9df120fa60cab5163dcb1be913d3fe4d25f3f980
Matheus Bras <bras.matheus@gmail.com>
2014-02-03 13:49:55 -0200

    Fix Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
           style: 'compressed'
         },
         files: {
-          'style.css': 'sass/style.scss'
+          'stylesheets/style.css': 'sass/style.scss'
         }
       }
     },
