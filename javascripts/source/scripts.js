88bdaa43f967f2fe7f885dcf285870371ba1ff4a
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-02-10 14:30:58 -0200

    Removed phone mask

--- javascripts/source/scripts.js
+++ javascripts/source/scripts.js
@@ -117,19 +117,6 @@ var App = {
       $(this).attr("src", "/images/time/time-eduardo.gif");
     });
 
-    // contact form
-    $("#phone").focusout(function(){
-      var phone, element;
-      element = $(this);
-      element.unmask();
-      phone = element.val().replace(/\D/g, '');
-      if(phone.length > 10) {
-          element.mask("(99) 99999-999?9");
-      } else {
-          element.mask("(99) 9999-9999?9");
-      }
-    }).trigger('focusout');
-
     $("#talk-to-us").validate({
       rules: {
         Field3: {
