6e6cb85871d0fcfb6233d188288609991f4333d5
Matheus Bras <bras.matheus@gmail.com>
2014-03-10 10:41:59 -0300

    Remove sass task from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     watch: {
-      sass: {
-        files: ['sass/**'],
-        tasks: ['sass'],
-        options: {
-          livereload: true
-        }
-      },
       js: {
         files: ['javascripts/source/**'],
         tasks: ['uglify'],
@@ -18,29 +11,11 @@ module.exports = function(grunt) {
         }
       }
     },
-    sass: {
-      dist: {
-        options: {
-          style: 'compressed'
-        },
-        files: {
-          'stylesheets/style.css': 'sass/style.scss'
-        }
-      }
-    },
     uglify: {
       my_target: {
         files: {
           'javascripts/application.min.js': [
-            'javascripts/source/pace.js',
-            'javascripts/source/jquery.localScroll.js',
-            'javascripts/source/jquery.maskedinput.js',
-            'javascripts/source/jquery.prettyPhoto.js',
-            'javascripts/source/jquery.scrollTo.js',
-            'javascripts/source/jquery.tipTip.js',
-            'javascripts/source/jquery.validate.js',
-            'javascripts/source/instantclick.min.js',
-            'javascripts/source/scripts.js'
+            'javascripts/source/*.js'
           ]
         }
       }
@@ -49,12 +24,11 @@ module.exports = function(grunt) {
 
   //Load NPM tasks
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   //Making grunt default to force in order not to break the project.
   grunt.option('force', true);
 
   //Default task(s).
-  grunt.registerTask('default', ['sass', 'uglify', 'watch']);
+  grunt.registerTask('default', ['uglify', 'watch']);
 };
